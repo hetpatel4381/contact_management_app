be7155cc13dc61b307a23fff918a3b04c0554ca0
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-09 19:50:48 +0530

    contact controller is created

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,26 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getContacts,
+  createContact,
+  updateContact,
+  deleteContact,
+  getContact
+} = require("../controllers/contactController");
 
-router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Get All Contacts" });
-  // res.json({message: "Get All Contacts"});
-  // res.send("Get All Contacts");
-});
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-router.route("/").post((req, res) => {
-    res.status(200).json({message: "Create Contact"});
-});
-
-router.route("/:id").get((req, res) => {
-    res.status(200).json({message: `Get Contact for ${req.params.id}`});
-});
-
-router.route("/:id").put((req, res) => {
-    res.status(200).json({ message: `Update Contact for ${req.params.id}` });
-});
-
-router.route("/:id").delete((req, res) => {
-    res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
-});
-
-module.exports = router; 
+module.exports = router;
