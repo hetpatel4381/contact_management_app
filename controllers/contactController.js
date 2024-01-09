ad80812ecf1f1905ca6fd814658cf684db90ab11
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-09 23:05:38 +0530

    implemented middleware for json parsing as well as created the constants file

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,39 +1,47 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get ALl Contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get All Contacts" });
   // res.json({message: "Get All Contacts"});
   // res.send("Get All Contacts");
-};
+});
 
 // @desc Create New Contacts
 // @route POST /api/contacts
 // @access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body is", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
   res.status(201).json({ message: "Create Contact" });
-};
+});
 
 // @desc Get Contact
 // @route GET /api/contacts/:id
 // @access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get Contact for ${req.params.id}` });
-};
+});
 
 // @desc Update Contact
 // @route PUT /api/contacts/:id
 // @access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Contact for ${req.params.id}` });
-};
+});
 
 // @desc Delete Contact
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
