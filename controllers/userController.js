18ab2455f6d8d100a0c31cf0276804bacfa44c64
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-10 12:43:49 +0530

    JWT accessToken & user login is implemented with password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // @desc Register a User
@@ -13,14 +14,14 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const userAvailable = await User.findOne({ email });
-  if(userAvailable){
+  if (userAvailable) {
     res.status(400);
     throw new Error("User Already Registered!");
   }
 
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log('Hashed Password', hashedPassword);
+  console.log("Hashed Password", hashedPassword);
 
   const newUser = await User.create({
     username,
@@ -28,11 +29,11 @@ const registerUser = asyncHandler(async (req, res) => {
     password: hashedPassword,
   });
 
-  console.log("User Created Successfully: ",newUser);
+  console.log("User Created Successfully: ", newUser);
 
-  if(newUser){
-    res.status(201).json({_id: newUser.id, email: newUser.email});
-  }else {
+  if (newUser) {
+    res.status(201).json({ _id: newUser.id, email: newUser.email });
+  } else {
     res.status(400);
     throw new Error("User data is not valid");
   }
@@ -44,6 +45,32 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All Fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+  // compare password with hashedPassword
+  if (email && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {expiresIn: "1m"}
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.json(401);
+    throw new Error("Email or password is not valid");
+  }
+
   res.json({ message: "Login the user" });
 });
 
